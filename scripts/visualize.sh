7b32c35b505ed3ba266019b22f91408a41f06de2
Kzernobog <adityakamireddypalli@gmail.com>
2021-08-02 17:36:23 +0100

    saves experiment videos

--- scripts/visualize.sh
+++ scripts/visualize.sh
@@ -21,3 +21,13 @@ then
 fi
 
 ./rrc2021_latest.sif python3 ./scripts/trifinger_platform_log_viewer.py "${experiment_dir}/robot_data.dat" "${experiment_dir}/camera_data.dat" -g "${experiment_dir}/goal.json"
+
+./rrc2021_latest.sif ros2 run trifinger_object_tracking tricamera_log_converter \
+    "${experiment_dir}/camera_data.dat" "${experiment_dir}/video60.avi" -c camera60
+
+
+./rrc2021_latest.sif ros2 run trifinger_object_tracking tricamera_log_converter \
+    "${experiment_dir}/camera_data.dat" "${experiment_dir}/video180.avi" -c camera180
+
+./rrc2021_latest.sif ros2 run trifinger_object_tracking tricamera_log_converter \
+    "${experiment_dir}/camera_data.dat" "${experiment_dir}/video300.avi" -c camera300
