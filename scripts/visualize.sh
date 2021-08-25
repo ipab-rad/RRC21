a74141d611f5f6bcd270cbb3eb9788bc02ed9070
Kzernobog <adityakamireddypalli@gmail.com>
2021-08-25 18:23:34 +0100

    [DEBG] change the visualisation scripts
    Changed to visualise the dice experiment

--- scripts/visualize.sh
+++ scripts/visualize.sh
@@ -20,14 +20,16 @@ then
 	exit 1
 fi
 
-./rrc2021_latest.sif python3 ./scripts/trifinger_platform_log_viewer.py "${experiment_dir}/robot_data.dat" "${experiment_dir}/camera_data.dat" -g "${experiment_dir}/goal.json"
+# ./rrc2021_latest.sif python3 ./scripts/trifinger_platform_log_viewer.py "${experiment_dir}/robot_data.dat" "${experiment_dir}/camera_data.dat" -g "${experiment_dir}/goal.json"
 
-./rrc2021_latest.sif ros2 run trifinger_object_tracking tricamera_log_converter \
+# ./rrc2021_latest.sif ros2 run trifinger_object_tracking tricamera_log_converter \
+#     "${experiment_dir}/camera_data.dat" "${experiment_dir}/video60.avi" -c camera60
+# 
+# 
+# ./rrc2021_latest.sif ros2 run trifinger_object_tracking tricamera_log_converter \
+#     "${experiment_dir}/camera_data.dat" "${experiment_dir}/video180.avi" -c camera180
+# 
+# ./rrc2021_latest.sif ros2 run trifinger_object_tracking tricamera_log_converter \
+#     "${experiment_dir}/camera_data.dat" "${experiment_dir}/video300.avi" -c camera300
+./rrc2021_latest.sif ros2 run trifinger_cameras tricamera_log_converter \
     "${experiment_dir}/camera_data.dat" "${experiment_dir}/video60.avi" -c camera60
-
-
-./rrc2021_latest.sif ros2 run trifinger_object_tracking tricamera_log_converter \
-    "${experiment_dir}/camera_data.dat" "${experiment_dir}/video180.avi" -c camera180
-
-./rrc2021_latest.sif ros2 run trifinger_object_tracking tricamera_log_converter \
-    "${experiment_dir}/camera_data.dat" "${experiment_dir}/video300.avi" -c camera300
